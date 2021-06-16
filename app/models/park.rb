122e90f5de5d6d46a2cbec69f560475fd5fb7e87
Phillip Strom <phillip.strom@gmail.com>
2021-06-15 18:09:39 -0600

    Add image url to park

--- app/models/park.rb
+++ app/models/park.rb
@@ -2,6 +2,7 @@ class Park < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :directions, presence: true
+  validates :image_url, presence: true
 
   has_many :park_searches
   has_many :searches, through: :park_searches
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_225221) do
+ActiveRecord::Schema.define(version: 2021_06_16_000908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 2021_06_14_225221) do
     t.string "directions"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "latitude"
+    t.string "longitude"
+    t.string "image_url"
   end
 
   create_table "searches", force: :cascade do |t|
