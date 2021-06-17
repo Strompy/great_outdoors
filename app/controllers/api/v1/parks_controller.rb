f6c48416167cccd6d365e09fcd2ef055183afa8e
Phillip Strom <phillip.strom@gmail.com>
2021-06-17 11:00:40 -0600

    Add filter by name

--- app/controllers/api/v1/parks_controller.rb
+++ app/controllers/api/v1/parks_controller.rb
@@ -1,21 +1,20 @@
 class Api::V1::ParksController < ApplicationController
   def index
     if search_params[:location]
-      park = search_for_park(search_params[:location])
-      render json: ParkSerializer.new(park).serialized_json
+      parks = search_for_park(search_params[:location])
     elsif search_params[:order] == 'desc'
       parks = Park.order(created_at: :desc)
-      render json: ParkSerializer.new(parks).serialized_json
     elsif search_params[:order] == 'alpha_asc'
       parks = Park.order(:name)
-      render json: ParkSerializer.new(parks).serialized_json
     elsif search_params[:order] == 'alpha_desc'
       parks = Park.order(name: :desc)
-      render json: ParkSerializer.new(parks).serialized_json
     else
       parks = Park.all
-      render json: ParkSerializer.new(parks).serialized_json
     end
+    if search_params[:filter]
+      parks.where("name LIKE ?", search_params[:filter])
+    end
+    render json: ParkSerializer.new(parks).serialized_json
   end
 
   private
